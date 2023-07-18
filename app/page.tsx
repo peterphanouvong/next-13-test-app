8655b23bf332e5e63f1be3c19a9a68e1a38b369a
Peter Phanouvong <peterphanouvong@gmail.com>
2023-07-18 15:30:19 +1000

    blah

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import jwtDecode from "jwt-decode";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
   const pathname = usePathname();
-  const { isLoading, isAuthenticated, user, getToken } = useKindeAuth();
+  const {
+    isLoading,
+    isAuthenticated,
+    user,
+    getToken,
+    getPermissions,
+    getFlag,
+  } = useKindeAuth();
+
+  const [accessToken, setAccessToken] = useState();
 
   const handleGetTokenHello = async () => {
     const res = await fetch("/api/hello");
     const data = await res.json();
-    console.log(data);
+    setAccessToken(jwtDecode(data.access_token));
     // alert(JSON.stringify(data, null, 2));
   };
 
@@ -20,6 +31,22 @@ export default function Home() {
     const data = await res.json();
     alert(JSON.stringify(data, null, 2));
   };
+
+  const handleGetPermissions = () => {
+    console.log(getPermissions());
+  };
+
+  const handleGetFeatureFlags = () => {
+    console.log(getFlag);
+    console.log(getFlag("create:event"));
+  };
+
+  if (isLoading)
+    return (
+      <main className="flex min-h-screen flex-col gap-10 items-center justify-center p-24">
+        Loading...
+      </main>
+    );
 
   if (!isLoading && !isAuthenticated && pathname != "/") {
     return router.push("/");
@@ -32,16 +59,35 @@ export default function Home() {
           <Link
             prefetch={false}
             className="btn-primary mr-2"
-            href={{ pathname: "/api/auth/login" }}
+            href={{
+              pathname: "/api/auth/login",
+            }}
           >
             Login
           </Link>
+
           <Link
             prefetch={false}
             className="btn-primary"
-            href={{ pathname: "/api/auth/register" }}
+            href={{
+              pathname: "/api/auth/register",
+              query: {},
+            }}
           >
             Register
+          </Link>
+
+          <Link
+            prefetch={false}
+            className="btn-primary"
+            href={{
+              pathname: "/api/auth/create_org",
+              query: {
+                org_name: "Hello world 19",
+              },
+            }}
+          >
+            Create org
           </Link>
         </div>
       ) : (
@@ -50,12 +96,22 @@ export default function Home() {
             <button className="btn-secondary mr-4" onClick={handleGetTokenTest}>
               get token (api/test)
             </button>
-            <button className="btn-secondary" onClick={handleGetTokenHello}>
+            <button
+              className="btn-secondary mr-4"
+              onClick={handleGetTokenHello}
+            >
               get token (api/hello)
+            </button>
+            <button className="btn-secondary" onClick={handleGetPermissions}>
+              get permissions
+            </button>
+            <button className="btn-secondary" onClick={handleGetFeatureFlags}>
+              get ff
             </button>
           </div>
           <pre className="p-10 text-sm rounded-lg font-mono bg-gray-800 text-green-300 font-light">
             {JSON.stringify(user, null, 2)}
+            {accessToken ? JSON.stringify(accessToken, null, 2) : null}
           </pre>
           <Link
             prefetch={false}
---
+++ app/you-suck/page.tsx
@@ -0,0 +1,8 @@
+export default function YouSuck() {
+  return (
+    <main className="flex min-h-screen flex-col gap-5 items-center justify-center p-24">
+      <h1 className="text-9xl font-bold text-red-600">Error</h1>
+      <p className="text-3xl">You suck.</p>
+    </main>
+  );
+}
