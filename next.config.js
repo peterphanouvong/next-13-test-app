2a5259b2387674502c156f0f4f244cac7ca04688
Peter Phanouvong <peterphanouvong@gmail.com>
2024-03-19 16:08:00 +1100

    fads

--- next.config.js
+++ next.config.js
@@ -2,21 +2,16 @@
 const webpack = require("webpack");
 
 module.exports = {
-  webpack(config) {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        "process.env.KINDE_SITE_URL":
-          process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-        "process.env.KINDE_POST_LOGOUT_REDIRECT_URL":
-          process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
-          `https://${process.env.VERCEL_URL}`,
-        "process.env.KINDE_POST_LOGIN_REDIRECT_URL":
-          process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
-          `https://${process.env.VERCEL_URL}/onboarding`,
-      })
-    );
-
-    return config;
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ??
+      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/onboarding`,
   },
 };
 
